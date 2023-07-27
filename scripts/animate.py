b64f398547c914e23ff1eaeb8cca155e991fff30
Sanzhar <anapiyaevs0519@gmail.com>
2023-07-27 10:40:58 +0600

    [animate.py][L] - updated animation

--- scripts/animate.py
+++ scripts/animate.py
@@ -3,19 +3,6 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation, PillowWriter
 import numpy as np
 
-file = 'data/csv_expanded.csv'
-df = pd.read_csv(file)
-plt.style.use('dark_background')
-fig = plt.figure(figsize=(9,16))
-axes = fig.add_subplot(1,1,1)
-axes.set_ylim(0, 4200)
-axes.set_xlim(2007, 2050)
-plt.style.use("seaborn")
-
-
-x_1,y_1 = [], []
-y2 = []
-
 def formate_salary(salary):
     salary_str = str(salary)
     if len(salary_str)> 6:
@@ -25,21 +12,47 @@ def formate_salary(salary):
         res = salary_str[:-3] + ' ' + salary_str[-3:]
     return res
 
+file = 'data/csv_expanded2.csv'
+df = pd.read_csv(file)
+df['salary_avg'] = df['salary_avg'].astype('int')
+plt.style.use('dark_background')
+fig, (ax1, ax2) = plt.subplots(2, figsize=(9,16))
+# ax1 = fig.add_subplot(1,1,1)
+ax1.set_ylim(0, 30)
+ax1.set_xlim(2008, 2023)
 
+ax2.set_ylim(50000, 360000)
+ax2.set_xlim(2008, 2023)
+plt.style.use("seaborn")
+
+x_1,y_1 = [], []
+y2 = []
+y3 = []
 def animate(i):
     x_1.append(df['year_exp'][i])
-    y_1.append(df['inflation_growth'][i])
-    y2.append(df['salary_depend'][i])
-    plt.plot(x_1, y_1, color='yellow', label = 'Рост инфляции(%)')
-    plt.plot(x_1, y2,color='#E6825D', label = 'Рост Зарплат(%)')
-    plt.plot([], [], ' ', label=f"Год: {df['year'][i]}")
-    plt.plot([], [], ' ', label=f"Рост инфляции: {round(df['inflation'][i],2)}%")
-    plt.plot([], [], ' ', label=f"Средняя зарплата: {formate_salary(df['salary_avg'][i])} тенге")
-    plt.legend(['Рост инфляции(%)', 'Рост Зарплат(%)', f"Год: {df['year'][i]}", f"Рост инфляции: {round(df['inflation'][i],2)}%", f"Средняя зарплата: {formate_salary(df['salary_avg'][i])}"], loc ="upper left")
+    y_1.append(df['inflation'][i])
+    y2.append(df['salary_growth'][i])
+
+
+    ax1.plot(x_1, y_1, color='yellow', label = 'Рост инфляции(%)')
+    ax1.plot(x_1, y2,color='#E6825D', label = 'Рост Зарплат(%)')
+
+
+    ax1.plot([], [], ' ', label=f"Год: {df['year'][i]}")
+    ax1.plot([], [], ' ', label=f"Рост инфляции: {round(df['inflation'][i],2)}%")
+    ax1.legend(['Рост инфляции(%)', 'Рост Зарплат(%)', f"Год: {df['year'][i]}", f"Рост инфляции: {round(df['inflation'][i],2)}%"], loc ="upper left")
     for edge in ['top', 'right']:
-        axes.spines[edge].set_visible(False)
-    axes.tick_params(left = False)
-    plt.suptitle('Показатели инфляции и средней зарплаты с 2007 по 2050', size=17, weight='bold')
+        ax1.spines[edge].set_visible(False)
+    ax1.tick_params(left = False)
+    ax1.set_title('Прирост зарплаты и инфляции по годам', size=17, weight='bold')
+
+    y3.append(df['salary_avg'][i])
+
+    ax2.plot(x_1, y3,color='blue', label = 'Средняя зарплата')
+    ax2.plot([], [], ' ', label=f"Средняя зарплата: {formate_salary(df['salary_avg'][i])} тенге")
+    ax2.legend(['Средняя зарплата в тенге', f"Средняя зарплата: {formate_salary(round(df['salary_avg'][i],-2))}"], loc ="upper left")
+    ax2.set_title('Средняя зарплата в Казахстане', size=17, weight='bold')
+    
     #plt.suptitle(f"Рост инфлияции и средней зарплаты по сравнению с 2007 годом на {df['year'][i]} год", size= 14, weight='bold')
     #plt.text(2010, 3500, f"Год: {df['year'][i]}", bbox = {'facecolor': 'black', 'alpha': 0.5, 'pad': 8,  'ec': 'black'}, fontsize= 15)
     
@@ -52,8 +65,3 @@ animation = FuncAnimation(fig,animate, frames=range(0, len(df)+1), interval = 5)
 # animation.save('inflation_gif.gif', dpi=100, writer=PillowWriter(fps=150)) # Script for saving
 # plt.legend()
 plt.show()
-
-
-
-# plt.legend()
-# plt.show()
